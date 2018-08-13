cfbad10e5fea5422f36b7fa7bdfd72cc4ba7a4cb
Mary Francis <maryfsc@disroot.org>
2018-08-13 19:49:00 -0300

    Tentando implementar autenticação com email e senha firebase (have issues)

---
+++ main.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+  $('#login-button').click(logInClick);
+  
+  $('#register-user-button').on('click', function() {
+    event.preventDefault();
+
+    var email = $('#register-email').val();
+    var password = $('#register-password').val();
+
+    registerUser(email, password);
+  };
+  
+});
+
+function logInClick(event) {
+  event.preventDefault();
+
+  var email = $('#sign-in-email').val();
+  var password = $('#sign-in-password').val();
+
+  logInUser(email, password);
+}
+
+// function registerClick(event) {
+//   event.preventDefault();
+
+//   var email = $('#register-email').val();
+//   var password = $('#register-password').val();
+
+//   registerUser(email, password);
+// }
+
+function logInUser(email, password) {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function(response) {
+      var userId = response.user.uid;
+      console.log('olá');
+      // adicionar a função que redireciona para a próxima tela (newsfeed?)
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+}
+
+
+function registerUser(email, password) {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function(response) {
+      console.log('oie');
+      // adicionar a função que redireciona para a próxima tela (newsfeed?) 
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(errorMessage);
+    });
+}
+
