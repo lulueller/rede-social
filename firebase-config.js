6062067855de05d03b2794e73d50ad89b53a4ed8
Mary Francis <maryfsc@disroot.org>
2018-08-13 14:24:26 -0300

    Criação de arquivos base e formulário simples de login.

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCCskRKAnyWkR1GMJgKDSDtm08sAPMUl0Y",
+    authDomain: "rede-social-7f5fc.firebaseapp.com",
+    databaseURL: "https://rede-social-7f5fc.firebaseio.com",
+    projectId: "rede-social-7f5fc",
+    storageBucket: "",
+    messagingSenderId: "48180568487"
+  };
+  
+  firebase.initializeApp(config);
