e365e66724e65b2990cf3d37f26e2da5aa36fd34
Mary Francis <maryfsc@disroot.org>
2018-08-14 17:10:03 -0300

    Adicionando posts anteriores da base no perfil de cada usuário, e também os novos.

--- firebase-config.js
+++ firebase-config.js
@@ -4,8 +4,8 @@
     authDomain: "rede-social-7f5fc.firebaseapp.com",
     databaseURL: "https://rede-social-7f5fc.firebaseio.com",
     projectId: "rede-social-7f5fc",
-    storageBucket: "",
+    storageBucket: "rede-social-7f5fc.appspot.com",
     messagingSenderId: "48180568487"
   };
-  
+
   firebase.initializeApp(config);
--- profile.js
+++ profile.js
@@ -3,22 +3,42 @@ var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
 
 $(document).ready(function() {
 
-  $('#post-button').on('click', function() {
-    var newPost = $('#post-content').val();
+  getPostsFromDB();
+  $('#post-button').click(buttonClick);
 
-    $('#posts-container').append(`
-      <li>
-        <p>${newPost}</p>
-      </li>
-      `);
-
-    addPostToDB(newPost);
-  });
 });
 
+function buttonClick() {
+  var newPost = $('#post-content').val();
+  var postsFromDB = addPostToDB(newPost);
+
+  createPost(newPost, postsFromDB.key);
+}
+
+function getPostsFromDB() {
+  database.ref(USER_ID).once('value')
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+        var childKey = childSnapshot.key;
+        var childData = childSnapshot.val();
+
+        createPost(childData.content, childKey);
+      });
+    });
+}
+
 function addPostToDB(post) {
-  console.log('foi');
   return database.ref(USER_ID).push({
     content: post
   });
+}
+
+function createPost(content, key) {
+  var newPost = $('#post-content').val();
+
+  $('#posts-container').append(`
+      <li>
+        <p data-task-id=${key} >${content}</p>
+      </li>
+    `);
 }
