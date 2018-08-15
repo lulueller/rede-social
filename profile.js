09b033247f0a65ca1579067bedad0ef975eca101
Mary Francis <maryfsc@disroot.org>
2018-08-15 01:02:54 -0300

    Tentando implementar likes (em processo)

--- profile.js
+++ profile.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
 function buttonClick() {
   var newPost = $('#post-content').val();
   $('#post-content').val('');
-  
-  var postsFromDB = addPostToDB(newPost);
+
+  var postsFromDB = addPostToDB(newPost, 0);
 
   createPost(newPost, postsFromDB.key);
 }
@@ -24,24 +24,26 @@ function getPostsFromDB() {
         var childKey = childSnapshot.key;
         var childData = childSnapshot.val();
 
-        createPost(childData.content, childKey);
+        createPost(childData.content, childData.likeQuantity, childKey);
       });
     });
 }
 
-function addPostToDB(post) {
+function addPostToDB(post, likeQuantity) {
   return database.ref(USER_ID).push({
     content: post
+    likes: likeQuantity;
   });
 }
 
-function createPost(content, key) {
-  var newPost = $('#post-content').val();
+function createPost(content, likes, key) {
+  var likeCount = 0;
 
   $('#posts-container').append(`
     <li>
       <div data-post-id=${key} class="my-2">
         ${content}<br>
+        <button type="button" class="btn btn-light" data-like-id="${key}-like">${likeCount} Curtidas</button>
         <button type="button" class="btn btn-dark" data-button-id="${key}-bt">Excluir</button>
       </div>
     </li>
@@ -51,4 +53,8 @@ function createPost(content, key) {
     database.ref(USER_ID + "/" + key).remove();
     $(this).parents('li').remove();
   });
+
+  $(`button[data-like-id="${key}-like"]`).click(function() {
+    $(this).html(`${likeCount += 1} Curtidas`);
+  })
 }
