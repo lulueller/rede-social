39b117c39ea23f588b405409e0ac41066f8e35cd
Mary Francis <maryfsc@disroot.org>
2018-08-15 14:05:18 -0300

    Ajustando erros com o recebimento de likes.

--- profile.js
+++ profile.js
@@ -12,7 +12,7 @@ function buttonClick() {
   var newPost = $('#post-content').val();
   $('#post-content').val('');
 
-  var postsFromDB = addPostToDB(newPost, 0);
+  var postsFromDB = addPostToDB(newPost);
 
   createPost(newPost, postsFromDB.key);
 }
@@ -24,25 +24,24 @@ function getPostsFromDB() {
         var childKey = childSnapshot.key;
         var childData = childSnapshot.val();
 
-        createPost(childData.content, childKey);
+        createPost(childData.content, childData.likes, childKey);
       });
     });
 }
 
 function addPostToDB(post) {
   return database.ref(USER_ID).push({
-    content: post
+    content: post,
+    likes: 0
   });
 }
 
-function createPost(content, key) {
-  var likeCount = 0;
-
+function createPost(content, likes, key) {
   $('#posts-container').append(`
     <li>
       <div data-post-id=${key} class="my-2">
         <span data-content-id="${key}">${content}</span><br>
-        <button type="button" class="btn btn-light" data-like-id="${key}-like">${likeCount} Curtidas</button>
+        <button type="button" class="btn btn-light" data-like-id="${key}-like">${likes} Curtidas</button>
         <button type="button" class="btn btn-dark" data-button-id="${key}-ed">Editar</button>
         <button type="button" class="btn btn-dark" data-button-id="${key}-bt">Excluir</button>
       </div>
@@ -57,12 +56,20 @@ function createPost(content, key) {
   $(`button[data-button-id="${key}-id"]`).click(function() {
     var editedContent = prompt(`Editando esse post: ${content}`);
     $(`span[data-content-id=${key}]`).html(editedContent);
+    return database.ref(USER_ID + "/" + key).update({
+      content: editedContent
+    });
   });
 
   $(`button[data-like-id="${key}-like"]`).click(function() {
-    $(this).html(`${likeCount += 1} Curtidas`);
-    return database.ref(USER_ID + "/" + key).update({
-      likes: likeCount
-    });
+
+    $(this).html(`${likes += 1} Curtidas`);
+
+    database.ref(USER_ID + "/" + key).once('value')
+      .then(function() {
+        return database.ref(USER_ID + "/" + key).update({
+        likes: likes
+        });    
+      });   
   });
 }
