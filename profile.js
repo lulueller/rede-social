ed95f1dd60191bfb73b807fe5a8aacc714f8f536
Mary Francis <maryfsc@disroot.org>
2018-08-15 11:25:48 -0300

    Implementando likes no HTML e no Firebase.

--- profile.js
+++ profile.js
@@ -24,26 +24,26 @@ function getPostsFromDB() {
         var childKey = childSnapshot.key;
         var childData = childSnapshot.val();
 
-        createPost(childData.content, childData.likeQuantity, childKey);
+        createPost(childData.content, childKey);
       });
     });
 }
 
-function addPostToDB(post, likeQuantity) {
+function addPostToDB(post) {
   return database.ref(USER_ID).push({
     content: post
-    likes: likeQuantity;
   });
 }
 
-function createPost(content, likes, key) {
+function createPost(content, key) {
   var likeCount = 0;
 
   $('#posts-container').append(`
     <li>
       <div data-post-id=${key} class="my-2">
-        ${content}<br>
+        <span data-content-id="${key}">${content}</span><br>
         <button type="button" class="btn btn-light" data-like-id="${key}-like">${likeCount} Curtidas</button>
+        <button type="button" class="btn btn-dark" data-button-id="${key}-ed">Editar</button>
         <button type="button" class="btn btn-dark" data-button-id="${key}-bt">Excluir</button>
       </div>
     </li>
@@ -54,7 +54,15 @@ function createPost(content, likes, key) {
     $(this).parents('li').remove();
   });
 
+  $(`button[data-button-id="${key}-id"]`).click(function() {
+    var editedContent = prompt(`Editando esse post: ${content}`);
+    $(`span[data-content-id=${key}]`).html(editedContent);
+  });
+
   $(`button[data-like-id="${key}-like"]`).click(function() {
     $(this).html(`${likeCount += 1} Curtidas`);
-  })
+    return database.ref(USER_ID + "/" + key).update({
+      likes: likeCount
+    });
+  });
 }
